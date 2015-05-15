60a6d772cd41ebc6b414fa352bab5c2dd619dff4
Thomas Levine <_@thomaslevine.com>
2015-05-15 07:02:45 +0000

    -

--- download/vocopvarenden/__init__.py
+++ download/vocopvarenden/__init__.py
@@ -4,12 +4,13 @@ import vlermv
 
 from . import download
 
-today = datetime.date.today()
-db = vlermv.Vlermv('data/vocopvarenden', mutable = False)
+def main():
+    today = datetime.date.today()
+    db = vlermv.Vlermv('data/vocopvarenden', mutable = False)
 
-search_response = download.search(today)
-year = 1640
-if year not in db:
-    list_response = download.list(year, search_response = search_response)
-    exportcsv_response = download.exportcsv(year, list_response = list_response)
-    db[year] = exportcsv_response.text
+    search_response = download.search(today)
+    year = 1640
+    if year not in db:
+        list_response = download.list(year, search_response = search_response)
+        exportcsv_response = download.exportcsv(year, list_response = list_response)
+        db[year] = exportcsv_response.text
