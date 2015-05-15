2078e85ce9a59741ef19338304243c0f9830e74d
Thomas Levine <_@thomaslevine.com>
2015-05-15 07:02:22 +0000

    -

--- download/vocopvarenden/download.py
+++ download/vocopvarenden/download.py
@@ -19,12 +19,12 @@ def list(year, search_response = None):
         raise TypeError('You must pass a search_response.')
     viewstate, viewstategenerator = parse.viewstate(search_response)
     return requests.post(_list.url, headers = _list.headers,
-               cookies = search_response.cookies
+               cookies = search_response.cookies,
                data = _list.data(viewstate, viewstategenerator, year))
 
 @cache(DIR, 'exportcsv')
 def exportcsv(year, list_response = None):
     if not list_response:
         raise TypeError('You must pass a list_response.')
-    return requests.get(_exportcsv.url, headers = headers,
+    return requests.get(_exportcsv.url, headers = _exportcsv.headers,
                         cookies = list_response.cookies)
