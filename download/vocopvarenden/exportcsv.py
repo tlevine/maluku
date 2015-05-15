1ce0c5a409a8d0adc46dd30ea1e173bc8a2f1ea2
Thomas Levine <_@thomaslevine.com>
2015-05-15 07:17:08 +0000

    -

--- download/vocopvarenden/exportcsv.py
+++ download/vocopvarenden/exportcsv.py
@@ -1,11 +1,9 @@
 url = 'http://vocopvarenden.nationaalarchief.nl/exportCSV.aspx'
 
 headers = {
-    'Host': 'vocopvarenden.nationaalarchief.nl',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:37.0) Gecko/20100101 Firefox/37.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'fr-FR,en;q=0.8,sv;q=0.5,en-US;q=0.3',
     'Accept-Encoding': 'gzip, deflate',
     'Referer': 'http://vocopvarenden.nationaalarchief.nl/list.aspx',
-    'Connection': 'keep-alive',
 }
