fd525e4b4daf7c8d3f101ce9982e6cc9f9aa6921
Thomas Levine <_@thomaslevine.com>
2015-05-17 18:34:42 +0000

    -

---
+++ sources/voyages.R
@@ -0,0 +1,6 @@
+colClasses <- c('character', 'factor', 'factor', 'numeric', 'factor', 'numeric', 'factor',
+                'factor', 'character', 'factor', 'character', 'character', 'character',
+                'factor', 'factor', 'character', 'factor',
+                'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'factor')
+voyages <- read.csv('voyages.csv', colClasses = colClasses)
+
