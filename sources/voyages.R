527651f0af1f8e0c141aae8583780d4e92383036
Thomas Levine <_@thomaslevine.com>
2015-05-17 20:28:30 +0000

    -

--- sources/voyages.R
+++ sources/voyages.R
@@ -1,12 +1,27 @@
 voyages.raw <- read.csv('voyages.csv', stringsAsFactors = FALSE)
 voyages <- voyages.raw['Number']
 names(voyages) <- 'number'
+
+d <- function(x) strptime(x, '%d-%m-%Y')
+
 voyages$ship <- factor(voyages.raw$Name.of.ship)
 voyages$master <- factor(voyages.raw$Master)
 voyages$tonnage <- as.numeric(sapply(strsplit(voyages.raw$Tonnage, '/'),
                                      function(x) if (length(x) > 0) x[length(x)] else NA))
-  $ Type.of.ship                       : chr  "" "pinas" "" "" ...
-   $ Built                              : chr  "1594" "1594" "1594" "1594" ...
-   $ Yard                               : chr  "Amsterdam" "Amsterdam" "Amsterdam" "Amsterdam" ...
-    $ Chamber                            : chr  "" "" "" "" ...
-    $ Date.of.departure                  : chr  "02-04-1595" "02-04-1595" "02-04-1595" "0
+voyages$ship.type <- factor(voyages.raw$Type.of.ship)
+voyages$built <- as.numeric(voyages.raw$Built)
+voyages$yard <- as.numeric(voyages.raw$Yard)
+voyages$chamber <- as.numeric(voyages.raw$Chamber)
+voyages$departure.date <- d(voyages.raw$Date.of.departure)
+voyages$departure.place <- factor(voyages.raw$Place.of.departure)
+voyages$cape.arrival.date <- d(voyages.raw$Arrival.at.Cape)
+voyages$cape.departure.date <- d(voyages.raw$Departure.from.Cape)
+voyages$arrival.date <- d(voyages.raw$Date.of.arrival.at.destination)
+voyages$destination.chamber <- factor(voyages.raw$Chamber.for.which.cargo.is.destined)
+voyages$particulars <- voyages.raw$Particulars
+voyages$onbcategory <- factor(voyages.raw$onbcategory)
+voyages$I <- as.numeric(voyages.raw$I)
+voyages$II <- as.numeric(voyages.raw$II)
+voyages$III <- as.numeric(voyages.raw$III)
+voyages$IV <- as.numeric(voyages.raw$IV)
+voyages$V <- as.numeric(voyages.raw$V)
